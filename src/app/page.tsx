9e460f081a9671431d585dacd7b52b8e6f128483
Simon Karman <mail@simonkarman.nl>
2024-05-16 00:18:27 +0200

    Tried adding FBX files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ const Home = () => {
     <>
       <Scene/>
       <div className="absolute top-0 left-0">
-        <div className="p-2 -space-y-1">
-          <h1 className="font-bold">My Board Game</h1>
-          <h2 className="font-medium text-gray-500">Lobby Phase</h2>
+        <div className="px-3 py-2 -space-y-1">
+          <h1 className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] font-bold text-2xl">My Board Game</h1>
+          <h2 className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] font-medium text-lg text-gray-300">Lobby Phase</h2>
         </div>
       </div>
     </>
--- src/components/scene.tsx
+++ src/components/scene.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { Canvas, useFrame, useLoader } from '@react-three/fiber';
-import { Grid, CameraControls, Plane } from '@react-three/drei';
-import { useRef } from 'react';
+import { Grid, CameraControls, Plane, Sphere } from '@react-three/drei';
+import { Suspense, useRef } from 'react';
 import { Box3, TextureLoader, Vector3 } from 'three';
+import { FBXLoader } from 'three-stdlib';
 
 export default function App() {
   return (
@@ -20,17 +21,18 @@ export default function App() {
 
 function Scene() {
   const ccRef = useRef<CameraControls | null>(null);
+  const targetRef = useRef<Sphere | null>(null);
 
   useFrame(() => {
     ccRef.current?.setBoundary(new Box3(
       new Vector3( -2.5, 0,-2.5 ),
       new Vector3( 2.5, 0, 2.5 )
     ));
+    ccRef.current?.getTarget(targetRef.current?.position);
   });
 
   return (
     <>
-      <ambientLight />
       <CameraControls
         ref={ccRef}
         verticalDragToForward={true}
@@ -39,26 +41,49 @@ function Scene() {
         maxDistance={7}
         minAzimuthAngle={-Math.PI / 6}
         maxAzimuthAngle={Math.PI / 6}
-        minPolarAngle={Math.PI / 6}
-        maxPolarAngle={Math.PI / 3}
+        minPolarAngle={Math.PI / 8}
+        maxPolarAngle={Math.PI / 2 - Math.PI / 8}
         dollySpeed={0.25}
         truckSpeed={2}
       />
-      <TableTop />
+      <ambientLight/>
+      <Sphere ref={targetRef} position={[0, 0, 0]} args={[0.025, 32, 32]} />
+      <Void/>
+      <Suspense>
+        <TableTop/>
+      </Suspense>
+      <Suspense>
+        <Card/>
+      </Suspense>
     </>
   )
 }
 
-function TableTop() {
-  const texture = useLoader(TextureLoader, '/paper/Paper006_2K-PNG_Color.png');
-  const normal = useLoader(TextureLoader, '/paper/Paper006_2K-PNG_NormalDX.png');
-  const displacement = useLoader(TextureLoader, '/paper/Paper006_2K-PNG_Displacement.png');
-  const roughness = useLoader(TextureLoader, '/paper/Paper006_2K-PNG_Roughness.png');
-  // const texture = useLoader(TextureLoader, '/fabric/Fabric039_2K-PNG_Color.png');
-  // const normal = useLoader(TextureLoader, '/fabric/Fabric039_2K-PNG_NormalDX.png');
-  // const displacement = useLoader(TextureLoader, '/fabric/Fabric039_2K-PNG_Displacement.png');
-  // const roughness = useLoader(TextureLoader, '/fabric/Fabric039_2K-PNG_Roughness.png');
+function Card() {
+  const model = useLoader(FBXLoader, '/cards/playingCards.fbx');
+  const texture = useLoader(TextureLoader, '/cards/playingCards_Geo_playingCards_Mat_baseColo.png');
+  const normal = useLoader(TextureLoader, '/cards/playingCards_Geo_playingCards_Mat_Normal.1.png');
+  const card = model.getObjectByName("diamonds")?.getObjectByName("diamonds01");
+  if (card) {
+    card.castShadow = true;
+    card.receiveShadow = true;
+  }
+  useFrame(() => {
+    if (card) {
+      card.rotation.y += 0.01;
+    }
+  });
+  return (<group position={card?.position.multiplyScalar(-0.01)} scale={[0.1, 0.1, 0.1]}>
+    <primitive object={card}>
+      <meshStandardMaterial
+        map={texture}
+        normalMap={normal}
+      />
+    </primitive>
+  </group>);
+}
 
+function Void() {
   const gridConfig = {
     cellSize: 0.5,
     cellThickness: 1,
@@ -71,17 +96,26 @@ function TableTop() {
     followCamera: false,
     infiniteGrid: false
   }
+  return <Grid position={[0, -0.02, 0]} args={[10.5, 10.5]} {...gridConfig} />;
+}
+
+function TableTop() {
+  // const texture = useLoader(TextureLoader, '/paper/Paper006_2K-PNG_Color.png');
+  // const normal = useLoader(TextureLoader, '/paper/Paper006_2K-PNG_NormalDX.png');
+  // const roughness = useLoader(TextureLoader, '/paper/Paper006_2K-PNG_Roughness.png');
+  const texture = useLoader(TextureLoader, '/fabric/Fabric039_2K-PNG_Color.png');
+  const normal = useLoader(TextureLoader, '/fabric/Fabric039_2K-PNG_NormalDX.png');
+  const roughness = useLoader(TextureLoader, '/fabric/Fabric039_2K-PNG_Roughness.png');
+
   return (<>
-    <Grid position={[0, -0.01, 0]} args={[10.5, 10.5]} {...gridConfig} />
     <Plane
       args={[5, 5]}
-      position={[0, 0, 0]}
+      position={[0, -0.01, 0]}
       rotation={[-Math.PI / 2, 0, 0]}
     >
       <meshStandardMaterial
         map={texture}
         normalMap={normal}
-        displacementMap={displacement}
         roughnessMap={roughness}
       />
     </Plane>
